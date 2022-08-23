45d1cb048b2ba58239fa0586b9568cabbbfba215
AndresIaconis <iaconisandres@gmail.com>
2022-08-23 18:05:31 -0300

    Entrega Menu

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import * as React from 'react'
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <NavBar />
+      </div>
+
+
+
+      <div>
+        Body
+      </div>
+
+
+
+
+      <div>
+        Footer
+      </div>
+
+      </div>
+
+    );
+}
+
+export default App;
